0b0201bed7a71342cd8a383f9dbbaf4d368c3cd0
Vu Dang Khoa <87051112+khoavukhoa3004@users.noreply.github.com>
2023-04-11 09:16:04 +0700

    modified:   Assets/Photon/PhotonUnityNetworking/Resources/PhotonServerSettings.asset 	modified:   Assets/Scenes/MainMenu.unity 	modified:   Assets/Scripts/NetworkManager.cs 	modified:   Assets/Scripts/UI/MainMenu.cs 	modified:   Assets/TextMesh Pro/Examples & Extras/Resources/Fonts & Materials/Bangers SDF.asset 	modified:   ProjectSettings/UnityConnectSettings.asset 	modified:   UserSettings/Layouts/CurrentMaximizeLayout.dwlt

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+
+
+    [Header("Dependencies")]
+	[SerializeField] private NetworkManager networkManager;
+
+
+    [Header("Texts")]
+	[SerializeField] private Text connectionStatus;
+
+    [SerializeField] private TMP_InputField nickNameField;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnConnect()
+    {
+        networkManager.Connect();
+    }
+
+    public void SetConnectionStatusText(string status)
+	{
+		connectionStatus.text = status;
+	}
+
+    public string getNickName()
+    {
+        return nickNameField.text;
     }
 }
